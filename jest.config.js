0e3f3638576c91510a5b3bf994a2fa041580fe58
Fernando De Caro <fernando.decaro@mercadolibre.com>
2019-07-11 17:17:40 -0300

    add tes

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  setupFiles: [
+    './jest.setup.js',
+  ],
+  testMatch: [
+    '<rootDir>/src/app/components/**/__test__/?(*.)+(spec|test).js?(x)',
+    '<rootDir>/src/app/page/**/__test__/?(*.)+(spec|test).js?(x)',
+  ],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer',
+  ],
+  verbose: true,
+  // testPathIgnorePatterns: ['<rootDir>/tests/e2e'],
+  testEnvironment: 'node',
+};
